6ff04685111d224b9453ab1d4902a83a39c7f9ad
Edoardo Tarulli <S1094923@studenti.univpm.it>
2022-06-01 12:49:41 +0200

    Aggiornamenti

---
+++ resources/js/backbtn.js
@@ -0,0 +1,14 @@
+
+const backBtn = document.getElementById("backBtn");
+backBtn.addEventListener('click', function() {
+    window.scrollTo(0,0);
+});
+
+window.addEventListener('scroll', function(){
+    let posY = window.scrollY;   
+    if(posY > 200) { 
+        backBtn.style.display= 'block';
+    } else {
+        backBtn.style.display= 'none';
+    }
+});
