e03f4ed3c567d4b1aa8b3799063c0c9b13996d25
Edoardo Tarulli <S1094923@studenti.univpm.it>
2022-06-04 16:01:45 +0200

    Schermata area riservata admin e la sua area pubblica, miglioramento footer

--- resources/views/layouts/-footer.blade.php
+++ resources/views/layouts/-footer.blade.php
@@ -9,7 +9,7 @@
         <h2 class="small-footer-header">website</h2>
         <p class="footer-support-text">
           <a class="footer-link" href="https://www.google.it/maps/place/Aula+Azzurra,+Polo+Montedago/@43.5851392,13.5127924,17z/data=!3m1!4b1!4m5!3m4!1s0x132d803ca6f93f49:0xf726911189934da!8m2!3d43.5851353!4d13.5149811"
-           target="_blank">Dove siamo</a>
+          target="_blank">Dove siamo</a>
         </p>
         <p class="footer-support-text">
           <a class="footer-link" href="" target="_self">Come operiamo</a>
