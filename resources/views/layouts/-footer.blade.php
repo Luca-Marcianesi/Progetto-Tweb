d0a989e0cd62114c054c2f07e40c5673f82ac423
Edoardo Tarulli <S1094923@studenti.univpm.it>
2022-06-07 19:35:22 +0200

    CSS, html e js dei popup

--- resources/views/layouts/-footer.blade.php
+++ resources/views/layouts/-footer.blade.php
@@ -1,5 +1,4 @@
-<link href="popup.js" rel="stylesheet">
-<script defer src="script.js"></script>
+<script defer src="/js/popup.js"></script>
 <div>  
   <footer class="footer">
     <div class="row footer-container">
@@ -8,22 +7,88 @@
         <p class="footer-support-text">We are an independent local community and study group</p>
       </div>
       <div class="column column-2">
-        <h2 class="small-footer-header">website</h2>
+        <h2 class="small-footer-header">informazioni utili</h2>
         <p class="footer-support-text">
           <a class="footer-link" href="https://www.google.it/maps/place/Aula+Azzurra,+Polo+Montedago/@43.5851392,13.5127924,17z/data=!3m1!4b1!4m5!3m4!1s0x132d803ca6f93f49:0xf726911189934da!8m2!3d43.5851353!4d13.5149811"
           target="_blank">Dove siamo</a>
         </p>
         <p class="footer-support-text">
-          <a class="footer-link" href="" target="_self">Come operiamo</a>
+        <text class="footer-link" data-modal-target="#modal1">Come operiamo</text>
+          <div class="modal" id="modal1">
+            <div class="modal-header">
+              <div class="title">Come operiamo</div>
+              <button data-close-button class="close-button">x</button>
+            </div>
+            <div class="modal-body">
+              AlloggiStudenti.com è un sito web creato da noi, un gruppo di 4 ragazzi dell'università
+              politecnica delle Marche. <font color ="FF974B">Qual'è il nostro obbiettivo?</font> 
+              Quello di permettere agli studenti universitari e non solo di poter trovare l'alloggi
+              più adatto per rendere il proprio percorso formativo il più piacevole e meno stressante possibile.
+              <font color = "FF974B">Come funziona il nostro servizio?</font> Noi cerchiamo di fornirti nella maniera più intuitiva
+              e efficace possibile, una serie di alloggi di diversa tipologia in diverse città del 
+              territorio italiano, in modo da fornirti un ampia scelta per poter soddifsare le tue 
+              necessità. Tutto questo viene supportato da una sezione di messaggistica per poter interagire 
+              direttamente con il proprietario dell'alloggio e, dalla parte "opposta", dal proprietario
+              verso la persona che desidera affittare l'alloggio così da chiarire eventuali dubbi e perplessità
+              parlando con la persona perfettamente adatta a rispondere. Noi ci impegniamo continuamente a 
+              migliorare il nostro sito web attraverso miglioramenti, introduzione di nuove funzionalità e 
+              features senza andare a complicarlo mantenendolo intuitivo e semplice. 
+            </div>
+          </div>
+          <div id="overlay"></div>
         </p>
       </div>
       <div class="column column-3">
-        <h2 class="small-footer-header">privacy</h2>
+        <h2 class="small-footer-header">tutela e privacy</h2>
         <p class="footer-support-text">
-          <a class="footer-link" href="" target="_blank">Informativa sulla privacy</a>
+          <text class="footer-link" data-modal-target="#modal2">Informativa sulla privacy</text>
+          <div class="modal" id="modal2">
+            <div class="modal-header">
+              <div class="title">Informativa sulla privacy</div>
+              <button data-close-button class="close-button">x</button>
+            </div>
+            <div class="modal-body">
+              Prima di tutto, la tua privacy per noi è fondamentale. Lo sappiamo che tutti fanno questa premessa, 
+              ma per noi vale davvero. Tu ci dai fiducia utilizzando i servizi di Alloggistudenti.com
+              e noi lo apprezziamo molto. Questo significa che una delle nostre priorità è quella di 
+              salvaguardare qualsiasi dato personale che ci fornisci e lo facciamo con impegno. 
+              Agiamo nel miglior interesse delle persone che si affidano a noi e con trasparenza a proposito
+              del trattamento dei tuoi dati personali. Qui descriviamo nella maniera più chiara e leggibile
+              possibile, come utilizziamo e trattiamo i tuoi dati personali. Per rendere il nostro servizio più
+              utile possibile e che soddisfi tutte le diverse necessita dei nostri clienti, quando utilizzi un nostro
+              servizio dobbiamo chiederti alcune cose come il tuo nome, la tua mail per poterti contattare, 
+              tutte informazioni di routine che devi inserire nel momento della registrazione per permetterci 
+              di rendere più efficace possibile il notro servizio. <font color = "FF974B">Perché tutto questo?</font>
+              Per permetterci di migliorare e aggiornare continuamente le nostre funzionalità 
+              e i nostri servizi in base alle tue preferenze, aiutarti a gestire le tue prenotazioni
+              e tramite esse, capire quali possano essere le promozioni e le offerte che potrebbero interessarti.
+            </div>
+          </div>
+          <div id="overlay"></div>
         </p>
         <p class="footer-support-text">
-            <a class="footer-link" href="" target="_blank">Info e risorse sulla sicurezza</a>
+        <text class="footer-link" data-modal-target="#modal3">I nostri valori</text>
+          <div class="modal" id="modal3">
+            <div class="modal-header">
+              <div class="title">I nostri valori</div>
+              <button data-close-button class="close-button">x</button>
+            </div>
+            <div class="modal-body">
+              Uno dei punti che ci teniamo a sottolineare è quello della <font color ="FF974B">Responsabilità</font>
+              nel viaggiare. Inanzitutto ci aspettiamo che le persone che interagiscono attraverso il nostro sito
+              mostrino il dovuto rispetto l'uno con l'altro. Non accettiamo alcuna forma di molestie, discriminazione,
+              incitamento all'odio, manipolazione, violenza fisica e altre forme di maltrattamento. Durante il soggiorno
+              presso una struttura o l'uso di un qualsiasi servizio di AlloggiStudenti.com, ti chiediamo di rispettare
+              la comunità locale. <font color ="FF974B">Ricorda di</font> limitare rumori che possono disturbare le persone vicine a te, seguire le
+              regole previste, le tradizioni del posto e di considerare sempre il tuo impatto sull'ambiente. <font color ="FF974B">Utilizza</font>
+              la nostra piattaforma in modo onesto e professionale, senza fornire descrizioni e frasi ingannevoli. <font color ="FF974B">Viaggia in sicurezza:</font>
+              AlloggiStudenti.com non tollera furti, vandalismo e attività criminali. <font color ="FF974B">Mantieni
+              la sicurezza della tua persona:</font> non intraprendere o incitare attività criminali che possano arrecare danni
+              ad altre persone e persino agli animali. Ti chiediamo di rispettare questi valori per te e per gli altri.<br>
+              <font color ="FF974B">Grazie</font> a nome di tutto lo staff di AlloggiStudenti.com
+            </div>
+          </div>
+          <div id="overlay"></div>
         </p>
       </div>
     </div>
